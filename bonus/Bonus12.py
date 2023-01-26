85f2a5c0191146cfde8be00dbb4d1541324dcec6
nagyosse <123080572+nagyosse@users.noreply.github.com>
2023-01-26 04:15:05 +0100

    doc strings 12

--- bonus/Bonus12.py
+++ bonus/Bonus12.py
@@ -1,17 +1,23 @@
 feet_inches = input("Enter feet and inches: ")
 
 
-def convert(feet_inches):
+def parse(feet_inches):
     parts = feet_inches.split(" ")
     feet = float(parts[0])
     inches = float(parts[1])
+    return {"feet": feet, "inches": inches}
 
+
+def convert(feet, inches):
     meters = feet * 0.3048 + inches * 0.0254
     return meters
 
 
-result = convert(feet_inches)
+parsed = parse(feet_inches)
 
+result = convert(parsed['feet'], parsed['inches'])
+
+print(f"{parsed['feet']} feet and {parsed['inches']} is equel to {result}")
 if result < 1:
     print("Kid is too small.")
 else:
