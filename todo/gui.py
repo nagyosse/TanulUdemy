bcd278161c4e96c9386a0d06a5a6057d37165b28
nagyosse <123080572+nagyosse@users.noreply.github.com>
2023-02-17 12:47:13 +0100

    Add button and add exit button

--- todo/gui.py
+++ todo/gui.py
@@ -3,9 +3,24 @@ import PySimpleGUI as sg
 
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("add")
 
 window = sg.Window('My to-do app', layout=[[label], [input_box, add_button]])
-window.read()
+
+while True:
+    event, value = window.read()
+    print(event)
+    print(value)
+    match event:
+        case "add":
+            todos = functions.get_todo()
+            new_todo = value["todo"] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
+
 window.close()
