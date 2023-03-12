1a67cc0d19e2c7974a0152ab5ceb3ca1b2ed46e5
nagyosse <123080572+nagyosse@users.noreply.github.com>
2023-03-12 03:45:45 +0100

    todo web app add complet

--- todo/web.py
+++ todo/web.py
@@ -12,10 +12,14 @@ st.title("My todo App")
 st.subheader("This is my todo app")
 st.write("This productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="add new todo...",
               on_change=add_todo, key='new_todo')
 
-st.session_state
