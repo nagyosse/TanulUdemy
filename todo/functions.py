9e9179bfa51273cc0fb7e1cc0143206a1557a9b7
nagyosse <123080572+nagyosse@users.noreply.github.com>
2023-03-18 21:47:20 +0100

    todo web app add complet 1

--- todo/functions.py
+++ todo/functions.py
@@ -13,7 +13,7 @@ def get_todos(filepath=FILEPATH):
 def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file. """
     with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
+        file.writelines(to dos_arg)
 
 
 print(__name__)
